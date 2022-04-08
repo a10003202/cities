2b9de49a25a45a32db652c559d705333d5b4233f
Angel Lizarraga <leonardo@192.168.1.70>
2022-04-08 15:46:23 -0500

    Remove minimum coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,6 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 
 SimpleCov.start('rails') do
-  minimum_coverage 50
   maximum_coverage_drop 2
 end
 WebMock.disable_net_connect!(allow_localhost: true)
