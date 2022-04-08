6ac0905ba378b8613089c26babf1e08e009f52c0
Angel Lizarraga <leonardo@192.168.1.70>
2022-04-08 11:10:50 -0500

    Added terminals test

---
+++ spec/requests/terminals_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :request do
+  describe 'GET /index' do
+    it 'redirect to login' do
+      get terminals_path
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
